fa893468fd4f68727465eba852d5c2c954c85b3c
Hugo Taillefumier <htaillef@z1r10p4.le-101.fr>
2017-11-10 11:32:54 +0100

    maj ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: htaillef <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 12:50:21 by htaillef          #+#    #+#             */
-/*   Updated: 2017/11/09 16:44:33 by htaillef         ###   ########.fr       */
+/*   Updated: 2017/11/10 11:31:47 by htaillef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && i < n && s1[i] == s2[i])
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		else if (!s1[i])
+			return (0);
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
